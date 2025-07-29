75d1eab858a6d8e340f0368a1cbf19b3916c2ac3
Adrian <adr1337@outlook.com>
2025-07-29 18:46:25 -0300

    Corrección de campo horario

--- back/models/classModel.js
+++ back/models/classModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
-const classChema = new mongoose.Schema({
+const classSchema = new mongoose.Schema({
     name: String,
-    schedule: String,
+    schedule: {
+        
+        dayOfWeek: { type: Number, required: true, enum: [1, 2, 3, 4, 5, 6] },
+        startTime: { type: String, required: true },
+        endTime: { type: String, required: true }
+
+    },
     teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
     tier: { type: mongoose.Schema.Types.ObjectId, ref: 'Tier' },
     description: String,
     active: { type: Boolean, default: true }
 });
 
-module.exports = mongoose.model('Class', classChema);
+module.exports = mongoose.model('Class', classSchema);
